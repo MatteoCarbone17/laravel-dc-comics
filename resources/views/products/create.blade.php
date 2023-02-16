724648791385495a0dbf3d1e4c6663929a327825
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-16 15:42:52 +0100

    add {{  old  }} in input

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -38,7 +38,7 @@
                         @endif
                         <div class="mb-3">
                           <label for="" class="form-label">Title</label>
-                          <input type="text" name="title" class="form-control" id="" >
+                          <input type="text" name=" title" class="form-control" value="{{ old('title')}}"  id="" >
                         </div>
                         @if (($errors->has('description')))
                         <div class="alert alert-danger">
@@ -68,7 +68,7 @@
                         @endif
                         <div class="mb-3">
                           <label for="" class="form-label">Link thumb img </label>
-                          <input type="text"  name="thumb" class="form-control" id="">
+                          <input type="text"  name="thumb" class="form-control"  value="{{ old('thumb')}}"  id="">
                         </div>
                         @if (($errors->has('price')))
                         <div class="alert alert-danger">
@@ -83,7 +83,7 @@
                         @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Price </label>
-                            <input type="text" name="price" class="form-control" id="">
+                            <input type="text" name="price" class="form-control" value="{{ old('price')}}"  id="">
                         </div>
                         @if (($errors->has('series')))
                         <div class="alert alert-danger">
@@ -98,7 +98,7 @@
                         @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Series </label>
-                            <input type="text" name="series" class="form-control" id="">
+                            <input type="text" name="series" class="form-control" value="{{ old('series')}}"  id="">
                         </div>
                         @if (($errors->has('sale_date')))
                         <div class="alert alert-danger">
@@ -113,7 +113,7 @@
                         @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Sale data</label>
-                            <input type="date" name="sale_date"  class="form-control" id="">
+                            <input type="date" name="sale_date"  class="form-control" value="{{ old('sale_date')}}"  id="">
                         </div>
                         @if (($errors->has('type')))
                         <div class="alert alert-danger">
@@ -128,7 +128,7 @@
                         @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Type </label>
-                            <input type="text" name="type"  class="form-control" id="">
+                            <input type="text" name="type"  class="form-control" value="{{ old('type')}}" id="">
                         </div>
                         <div class="col">
                             <button type="submit" class="btn btn-success">Save Comic</button>
