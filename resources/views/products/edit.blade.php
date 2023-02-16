4280e2c0ab755feca111f4af69343ae8fc06502b
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-16 15:52:59 +0100

    upgrade edit validation

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -30,31 +30,97 @@
                         @endif
                         <div class="mb-3">
                           <label for="" class="form-label">Title</label>
-                          <input type="text" name="title" class="form-control" id="" value="{{ $product->title }}" >
+                          <input type="text" name="title" class="form-control" id="" value="{{ old('title') ?? $product->title }}" >
                         </div>
+                        @if (($errors->has('description')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('description') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                             <label for="" class="form-label">description</label>
-                           <textarea name="description" id="" class="form-control">{{ $product->description }} </textarea>
-                          </div>
+                           <textarea name="description" id="" class="form-control">{{ old('description') ?? $product->description }} </textarea>
+                        </div>
+                        @if (($errors->has('thumb')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('thumb') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                           <label for="" class="form-label">Link thumb img </label>
-                          <input type="text"  name="thumb" class="form-control" id=""  value="{{ $product->thumb }}" >
+                          <input type="text"  name="thumb" class="form-control" id=""  value="{{ old('thumb') ?? $product->thumb }}" >
                         </div>
+                        @if (($errors->has('price')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('price') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Price </label>
-                            <input type="text" name="price" class="form-control" id="" value="{{ $product->price }}" >
+                            <input type="text" name="price" class="form-control" id="" value="{{ old('price') ??  $product->price }}" >
                         </div>
+                        @if (($errors->has('series')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('series') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Series </label>
-                            <input type="text" name="series" class="form-control"  value="{{ $product->series }}"  id="">
+                            <input type="text" name="series" class="form-control"  value="{{ old('series') ??  $product->series }}"  id="">
                         </div>
+                        @if (($errors->has('sale_date')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('sale_date') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Sale data</label>
-                            <input type="text" name="sale_date"  class="form-control" value="{{ $product->sale_date}}"  id="">
+                            <input type="text" name="sale_date"  class="form-control" value="{{  old('sale_date') ??  $product->sale_date}}"  id="">
                         </div>
+                        @if (($errors->has('type')))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->get('type') as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
                         <div class="mb-3">
                             <label for="" class="form-label">Type </label>
-                            <input type="text" name="type"  class="form-control" value="{{ $product->type}}"   id="">
+                            <input type="text" name="type"  class="form-control" value="{{ old('type') ??   $product->type}}"   id="">
                         </div>
                         <div class="col">
                             <button type="submit" class="btn btn-success">Save Comic</button>
